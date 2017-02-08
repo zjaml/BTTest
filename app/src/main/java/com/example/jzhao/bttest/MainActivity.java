544279e08285f4ded8b6ae77b6511fbb41e7caed
Jin Zhao <JZhao@tradestation.com>
2017-02-08 18:27:47 +0900

    check

--- app/src/main/java/com/example/jzhao/bttest/MainActivity.java
+++ app/src/main/java/com/example/jzhao/bttest/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.jzhao.bttest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.firebase.crash.FirebaseCrash;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FirebaseCrash.log("Activity created");
+        FirebaseCrash.logcat(Log.ERROR, TAG, "NPE caught");
+        FirebaseCrash.report(new Exception("Ah oh"));
         setContentView(R.layout.activity_main);
     }
 }
